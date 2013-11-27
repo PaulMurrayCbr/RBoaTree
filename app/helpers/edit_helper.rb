6bdbccbec27c71271f0462d3dc2dc30de9e80e5b
Paul Murray <pmurray@anbg.gov.au>
2013-11-27 15:52:24 +1100

    Ready to start on the node page, I think.

--- app/helpers/edit_helper.rb
+++ app/helpers/edit_helper.rb
@@ -1,2 +1,58 @@
 module EditHelper
+  def node_css(node)
+    css = 'node'
+    if !node
+      css << ' node-nil'
+    else
+      css = 'node'
+
+      if node.published?
+        css << ' node-published'
+      else
+        css << ' node-unpublished'
+      end
+
+      if node.draft?
+        css << ' node-draft'
+      else
+        css << ' node-final'
+      end
+
+      if node.next
+        css << ' node-replaced'
+      else
+        css << ' node-current'
+      end
+
+      # importnat stuff goes at the end, to override
+
+      if node.end?
+        css << ' node-end'
+      end
+
+      if node.root_of && node.root_of.workspace?
+        css << ' node-workspace-top'
+      end
+
+      if node.root_of && node.root_of.tree?
+        css << ' node-tree-top'
+      end
+
+    end
+    return css
+  end
+  
+  def tree_css(tree)
+    css = 'tree'
+    if !tree
+      css << ' tree-nil'
+    elsif tree.end?
+      css << ' tree-end'
+    elsif tree.workspace?
+      css << ' tree-workspace'
+    elsif tree.tree?
+      css << ' tree-tree'
+    end
+  end
+  
 end
