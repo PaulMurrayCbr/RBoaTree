8676cc89c6d567cfe1a6fb94a8fa24720f193b3e
Paul Murray <pmurray@anbg.gov.au>
2013-12-12 00:11:17 +1100

    Integrity checks moved into database

--- app/controllers/validate_data_controller.rb
+++ app/controllers/validate_data_controller.rb
@@ -12,47 +12,19 @@ class ValidateDataController < ApplicationController
 
     @results = Msg.new
 
-    msg = @results.add('self check')
-    msg.add 'default'
-    msg.ok 'ok'
-    msg.info 'info'
-    msg.warn 'warn'
-    msg.error 'error'
-    
-    
-    msg = @results.add('boatree_validate_sample_message')
-    
     Squirm.use(ActiveRecord::Base.connection.raw_connection) do
       begin
         results = ActiveRecord::Base.connection.execute('discard temp')
         db_raw_call 'boatree_validate_create_results_table'
-        db_raw_call 'boatree_validate_sample_message'
+        db_raw_call 'boatree_validate_all'
         results = ActiveRecord::Base.connection.execute('select * from validation_results order by id')
-        display_validation_results msg, results.to_enum
+        display_validation_results @results, results.to_enum
       rescue PG::RaiseException => e
         msg.error e.message
       rescue Exception => e
         msg.error e.to_s
       end
     end
-    
-    msg = @results.add('check tables')
-    tt(msg, 'Tree exists') { Tree.count }
-    tt(msg, 'TreeNode exists') { TreeNode.count }
-    tt(msg, 'TreeLink exists') { TreeLink.count }
-    msg = @results.add('database integrity checks')
-    check msg, :boatree_validate_no_op
-    check msg, :boatree_validate_error
-    check msg, :boatree_validate_end_node
-    msg = @results.add('TO BE REMOVED')
-    tt(msg, 'There should be a END tree') { Tree.find(0) }
-    tt(msg, 'There should be a END node') { TreeNode.find(0) }
-    ttt(msg, 'The node of the end tree should be the end node') { Tree.find(0).node == TreeNode.find(0) }
-    ttt(msg, 'End node should have no previous version') { TreeNode.find(0).copy_of == nil}
-    ttt(msg, 'End node should have no next version') { TreeNode.find(0).replacement == nil}
-    ttt(msg, 'End node should have no supernodes') { TreeNode.find(0).supernode_link.empty?}
-    ttt(msg, 'End node should have no subnodes') { TreeNode.find(0).subnode_link.empty?}
-    ttt(msg, 'End node should not be the prev of any node') { TreeNode.find(0).copies.empty?}
   end
 
   def display_validation_results(msg, results) 
@@ -82,38 +54,6 @@ class ValidateDataController < ApplicationController
         end    
       end
       msg.info results.next
-  end
-
-  def tt(msg, t, &b)
-    begin
-      r = b.call
-      msg.ok(t, r)
-    rescue  Exception => e
-      msg.error(t, e)
-    end
-  end
-
-  def check(msg, test_name)
-    begin
-      db_raw_call test_name
-      msg.ok test_name
-    rescue PG::RaiseException => e
-      msg.warn test_name, e.message.strip
-    rescue Exception => e
-      msg.error test_name, e.to_s
-    end
-  end
-
-  def ttt(msg, t, &b)
-    begin
-      if b.call
-        msg.ok(t)
-      else
-        msg.warn(t)
-      end
-    rescue  Exception => e
-      msg.error(t, e)
-    end
   end
 
 end
--- db/integrity_checks.sql
+++ db/integrity_checks.sql
@@ -16,18 +16,26 @@ end
 $$ 
 language plpgsql;
 
-create or replace function boatree_validate_msg(_msg varchar, _status char(1) = null) returns void as $$
+create or replace function boatree_validate_msg(_msg varchar, _status char(1) = null) returns integer as $$
+declare
+	_id integer;
 begin
 	insert into validation_results(created_at, updated_at, status, msg)
-	values ( localtimestamp, localtimestamp, _status, _msg);
+	values ( localtimestamp, localtimestamp, _status, _msg)
+	returning id into _id;
+	return _id;
 end 
 $$ 
 language plpgsql;
 
-create or replace function boatree_validate_msgblock(_msg varchar, _status char(1) = null) returns void as $$
+create or replace function boatree_validate_msgblock(_msg varchar, _status char(1) = null) returns integer as $$
+declare
+	_id integer;
 begin
 	insert into validation_results(created_at, updated_at, start_sublist, status, msg)
-	values ( localtimestamp, localtimestamp, true, _status, _msg);
+	values ( localtimestamp, localtimestamp, true, _status, _msg)
+	returning id into _id;
+	return _id;
 end 
 $$ 
 language plpgsql;
@@ -40,42 +48,97 @@ end
 $$ 
 language plpgsql;
 
-
-create or replace function boatree_validate_no_op() returns void as $$
+create or replace function boatree_validate_msgsetstatus(_id integer, _status char(1)) returns void as $$
 begin
-	raise notice 'Integrity check';	
+	update validation_results set status = _status where id = _id;
 end 
 $$ 
 language plpgsql;
 
-create or replace function boatree_validate_error() returns void as $$
+create or replace function boatree_validate_equalsi(_msg varchar,  _expected integer, _value integer) returns integer as $$
 begin
-	raise exception 'This check always returns an exception, to test the validation framework itself';	
+	if _value = _expected then
+		return boatree_validate_msg(_msg, 'k');
+	else
+		return boatree_validate_msg(_msg || ': expected ' || _expected || ', got ' || _value, 'w');
+	end if;
 end 
 $$ 
 language plpgsql;
 
-create or replace function boatree_validate_sample_message() returns void as $$
+create or replace function boatree_validate_nulli(_msg varchar,  _value integer) returns integer as $$
 begin
-		perform boatree_validate_msg('test message 1', null);
-		perform boatree_validate_msgblock('test start block', null);
-		perform boatree_validate_msg('test message 2', null);
-		perform boatree_validate_msgendblock();
-		perform boatree_validate_msgblock('test colors', null);
-		perform boatree_validate_msg('Ok', 'k');
-		perform boatree_validate_msg('Info', 'i');
-		perform boatree_validate_msg('Warn', 'w');
-		perform boatree_validate_msg('Err', 'e');
-		perform boatree_validate_msgendblock();
+	if _value is null then
+		return boatree_validate_msg(_msg, 'k');
+	else
+		return boatree_validate_msg(_msg || ': expected NULL, got ' || _value, 'w');
+	end if;
 end 
 $$ 
 language plpgsql;
 
+create or replace function boatree_validate_all() returns void as $$
+begin
+	perform boatree_validate_end_node();
+end 
+$$ 
+language plpgsql;
 
 
 create or replace function boatree_validate_end_node() returns void as $$
+declare
+	_block integer;
+	_zz integer;
 begin
-	raise exception 'TODO!';	
+	_block := boatree_validate_msgblock('boatree_validate_end_node');
+	begin
+		_zz := null;
+		select count(*) from tree where id = 0 into _zz;
+		perform boatree_validate_equalsi('there should be an end tree', 1, _zz);
+
+		_zz := null;
+		select count(*) from tree_node where id = 0 into _zz;
+		perform boatree_validate_equalsi('there should be an end node', 1, _zz);
+
+		_zz := null;
+		select tree_node_id from tree where id = 0 into _zz;
+		perform boatree_validate_equalsi('the tree node of the end tree should be end node', 0, _zz);
+		
+		_zz := null;
+		select tree_id from tree_node where id = 0 into _zz;
+		perform boatree_validate_equalsi('the tree of the end node should be the end tree', 0, _zz);
+
+		_zz := null;
+		select prev_node_id from tree_node where id = 0 into _zz;
+		perform boatree_validate_nulli('the end node should have no prev', _zz);
+
+		_zz := null;
+		select next_node_id from tree_node where id = 0 into _zz;
+		perform boatree_validate_nulli('the end node should have no next', _zz);
+
+		_zz := null;
+		select count(*) from tree_link where super_node_id = 0 into _zz;
+		perform boatree_validate_equalsi('the end node should have no subnodes', 0, _zz);
+
+		_zz := null;
+		select count(*) from tree_link where sub_node_id = 0 into _zz;
+		perform boatree_validate_msg(_zz || ' nodes have the end node as a subnode', 'i');
+
+		_zz := null;
+		select count(*) from tree_node where prev_node_id = 0 into _zz;
+		perform boatree_validate_equalsi('the end node should not be the prev of any node', 0, _zz);
+		
+		_zz := null;
+		select count(*) from tree_node where next_node_id = 0 into _zz;
+		perform boatree_validate_msg(_zz || ' nodes have the end node as a next node', 'i');
+
+		perform boatree_validate_msgsetstatus(_block, 'k');	
+	exception
+		when others then
+			perform boatree_validate_msg('ERR ' || sqlstate || ': ' || sqlerrm, 'e');
+			perform boatree_validate_msgsetstatus(_block, 'e');	
+	end; 
+	perform boatree_validate_msgendblock();
 end 
 $$ 
 language plpgsql;
